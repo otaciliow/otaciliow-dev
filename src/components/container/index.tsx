ca7d25d46548517310318858837c971346675334
Otacilio <otacilio.dml@gmail.com>
2025-05-15 16:07:35 -0300

    feat: add mobile rules

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -6,17 +6,14 @@ export function Container({children}: {children: ReactNode}) {
     const {theme, toggleTheme} = useTheme();
     return (
         <main className="w-full max-w-7xl mx-auto px-4 mt-4">
-            <div className={`absolute right-40 flex w-20 p-1 rounded-full items-center border transition-all ${theme === 'light' ? 'justify-start border-gray-900' : 'justify-end border-gray-100'}`}>
+            <div className={`absolute right-2 md:right-40 flex w-16 p-1 rounded-full items-center border transition-all ${theme === 'light' ? 'justify-start border-gray-900' : 'justify-end border-gray-100'}`}>
                 <button aria-label="theme switcher" className={`shadow-2xl p-1 rounded-full border`} onClick={toggleTheme}>
                     { theme === 'light' ? (
-                        <Sun size={20} className="text-gray-900"/>
+                        <Sun size={15} className="text-gray-900"/>
                     ) : (
-                        <Moon size={20} className='text-gray-100'/>
+                        <Moon size={15} className='text-gray-100'/>
                     )}
                 </button>
-                {/* <button aria-label="theme switcher" className={`shadow-2xl p-1 rounded-full border ${theme === 'dark' ? 'ml-auto' : 'opacity-0'}`} onClick={toggleTheme}>
-                    <Moon size={20}/>
-                </button> */}
             </div>
             {children}
         </main>
