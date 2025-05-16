6597fc1e952a7dab7fea1cb2ce5dfeb6a3675247
otaciliow <otacilio.dml@hotmail.com>
2025-05-16 17:33:49 -0300

    fix: theme switcher animation

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -6,14 +6,10 @@ export function Container({children}: {children: ReactNode}) {
     const {theme, toggleTheme} = useTheme();
     return (
         <main className="w-full max-w-7xl mx-auto px-4 mt-4">
-            <div className={`absolute right-2 md:right-40 flex w-16 p-1 rounded-full items-center border transition-all ${theme === 'light' ? 'justify-start border-gray-900' : 'justify-end border-gray-100'}`}>
-                <button aria-label="theme switcher" className={`shadow-2xl p-1 rounded-full border`} onClick={toggleTheme}>
-                    { theme === 'light' ? (
-                        <Sun size={15} className="text-gray-900"/>
-                    ) : (
-                        <Moon size={15} className='text-gray-100'/>
-                    )}
-                </button>
+            <div className={`absolute right-2 md:right-40 flex w-11 h-6 rounded-full shadow-inner items-center border border-purple-500 justify-around`}>
+                <Sun size={15} className="text-gray-100" onClick={toggleTheme}/>
+                <Moon size={15} className='text-gray-900' onClick={toggleTheme}/>
+                <button aria-label="theme switcher" className={`absolute shadow-md h-4 w-4 left-0.5 bg-purple-400 transition-transform duration-300 rounded-full ${theme === 'light' ? 'translate-x-0' : 'translate-x-5'}`} onClick={toggleTheme}></button>
             </div>
             {children}
         </main>
