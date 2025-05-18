b64ee1c6485422dd41f976da86cbc69a01d43517
otaciliow <otacilio.dml@hotmail.com>
2025-05-18 20:33:19 -0300

    feat: add header to layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,10 @@
-// import { Header } from '../header';
 import { Outlet } from 'react-router-dom';
+import { Header } from '../header';
 
 export function Layout() {
     return (
         <>
-            {/* <Header /> */}
+            <Header />
             <Outlet />
         </>
     )
