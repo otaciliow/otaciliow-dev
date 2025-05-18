cdd407b2dc01be20600742bc0ce52cbe43acd7f6
otaciliow <otacilio.dml@hotmail.com>
2025-05-18 20:33:04 -0300

    feat: add header component

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,16 +1,9 @@
 import { ReactNode } from "react";
-import { Sun, Moon } from 'lucide-react';
-import { useTheme } from '../../contexts/themeContext';
 
 export function Container({children}: {children: ReactNode}) {
-    const {theme, toggleTheme} = useTheme();
+
     return (
         <main className="w-full max-w-7xl mx-auto px-4 mt-4">
-            <div className={`absolute right-2 md:right-40 flex w-11 h-6 rounded-full shadow-inner items-center border border-purple-500 justify-around`}>
-                <Sun size={15} className="text-gray-100" onClick={toggleTheme}/>
-                <Moon size={15} className='text-gray-900' onClick={toggleTheme}/>
-                <button aria-label="theme switcher" className={`absolute shadow-md h-4 w-4 left-0.5 bg-purple-400 transition-transform duration-300 rounded-full ${theme === 'light' ? 'translate-x-0' : 'translate-x-5'}`} onClick={toggleTheme}></button>
-            </div>
             {children}
         </main>
     )
---
+++ src/components/header/index.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import { Sun, Moon } from 'lucide-react';
+import { useTheme } from '../../contexts/themeContext';
+
+export function Header() {
+    const [username, setUsername] = useState('<Otaciliow />')
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <header className="mx-auto p-5 flex justify-center shadow-2xl">
+            <div className="w-7xl flex items-center justify-between">
+                <h2 className="gradient-text text-2xl font-bold hover:animate-pulse transition-all">{username}</h2>
+                <div className="flex items-center gap-5">
+                    <a href="#about" className={`${theme === 'light' ? 'text-primary-600 hover:text-primary-500' : 'text-primary-500 hover:text-primary-100'} transition-colors`}>Sobre mim</a>
+                    <a href="#projects" className={`${theme === 'light' ? 'text-primary-600 hover:text-primary-500' : 'text-primary-500 hover:text-primary-100'} transition-colors`}>Projetos</a>
+                    <a href="#placeholder" className={`${theme === 'light' ? 'text-primary-600 hover:text-primary-500' : 'text-primary-500 hover:text-primary-100'} transition-colors`}>Placeholder</a>
+                </div>
+                <div className={`relative flex w-11 h-6 rounded-full cursor-pointer shadow-inner items-center border border-primary-500 justify-around`}>
+                    <Sun size={15} className="text-primary-100" onClick={toggleTheme}/>
+                    <Moon size={15} className='text-primary-600' onClick={toggleTheme}/>
+                    <button aria-label="theme switcher" className={`absolute cursor-pointer shadow-md h-4 w-4 left-0.5 bg-primary-500 transition-transform duration-300 rounded-full ${theme === 'light' ? 'translate-x-0' : 'translate-x-5'}`} onClick={toggleTheme}></button>
+                </div>
+            </div>
+        </header>
+    )
+}
