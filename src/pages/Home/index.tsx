352256d3d930bff93844177a5532c255eba5f746
Otacilio <otacilio.dml@gmail.com>
2025-05-19 12:07:25 -0300

    feat: add swiper setup; add swiper to projects' section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,13 @@ import { useState, useEffect, useContext } from 'react';
 import { db } from '../../services/firebaseConnection';
 import { collection, onSnapshot } from 'firebase/firestore';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/scrollbar';
+import 'swiper/css/pagination';
+
 import { UserContext } from '../../contexts/userContext';
 
 import { ReposProps } from "../../interfaces/IReposProps"
@@ -58,10 +65,25 @@ export function Home() {
                 </div>
             </section>
             <section id="projects" className="py-5 my-5 flex flex-col gap-10">
-                <h2 id="projects" className="font-bold text-center text-3xl text-primary-100">Projetos:</h2>
-                {repos.map((repo, i) => (
-                    <h2 key={i}>{repo.name}</h2>
-                ))}
+                <h2 id="projects" className="font-bold text-center text-3xl text-primary-100">Projetos</h2>
+                <Swiper
+                    modules={[Mousewheel, Pagination, Autoplay]}
+                    spaceBetween={30}
+                    slidesPerView={2}
+                    slidesPerGroup={2}
+                    mousewheel={true}
+                    pagination={{clickable: true}}
+                    autoplay={{delay: 6000, pauseOnMouseEnter: true}}
+                    className="w-full"
+                >
+                    {repos.map((repo, i) => (
+                        <SwiperSlide className="py-5">
+                            <div className="w-full flex items-center justify-center">
+                                <h2 key={i} className="text-center p-5 bg-primary-500 w-xl rounded-md">{repo.name}</h2>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </section>
         </Container>
     )
