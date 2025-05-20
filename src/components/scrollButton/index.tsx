af0e914b6a89a32b78cc6bb8a19b89336c5ba63d
Otacilio <otacilio.dml@gmail.com>
2025-05-20 11:19:59 -0300

    fix: scroll button's z-index

--- src/components/scrollButton/index.tsx
+++ src/components/scrollButton/index.tsx
@@ -19,7 +19,7 @@ export function ScrollToTopButton() {
     }
 
     return (
-        <div className={`flex flex-col items-center fixed bottom-15 right-10 z-50 transition-all ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+        <div className={`flex flex-col items-center fixed bottom-15 right-10 z-49 transition-all ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
             <button onClick={scrollToTop} className={`p-3 rounded-full bg-primary-500 text-white shadow-lg hover:bg-primary-600 cursor-pointer`} aria-label="voltar ao topo">
                 <ArrowUp size={30} />
             </button>
