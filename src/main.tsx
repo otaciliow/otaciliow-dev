b0e4e3932eba139fd909ebfd2c3e9585563d8d7c
Otacilio <otacilio.dml@gmail.com>
2025-05-22 15:15:09 -0300

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './App'
 import AuthProvider from './contexts/authContext'
 import ThemeProvider from './contexts/themeContext'
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,5 +14,6 @@ createRoot(document.getElementById('root')!).render(
         <RouterProvider router={router} />
       </ThemeProvider>
     </AuthProvider>
+    <Analytics />
   </StrictMode>,
 )
